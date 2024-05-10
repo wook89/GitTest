6db789f44f6ffa526bbf49c8d6745e9f3bfd859b
Wook89 <guddnr007@naver.com>
2024-05-10 15:18:12 +0900

    test

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -3,7 +3,7 @@ package test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git World~~~~~");
+		System.out.println("Hello Git World~~~~~!!");
 	}
 
 }
